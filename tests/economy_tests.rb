484f45e76f69a07111143cfbb2471298c9c649a2
amclees <andrewmclees5@gmail.com>
2017-04-30 17:53:30 -0700

    Add currency system tests

--- tests/economy_tests.rb
+++ tests/economy_tests.rb
@@ -2,3 +2,80 @@
 
 require 'simplecov'
 SimpleCov.start
+
+require_relative '../world/economy/currency_system.rb'
+
+# Tests CurrencySystem including parsing and custom system definitions
+class TestCurrencySystem < Test::Unit::TestCase
+  def test_value_storage
+    main = CurrencySystem.new
+    assert_equal(1, main.value(:cp))
+    assert_equal(10, main.value(:sp))
+    assert_equal(100, main.value(:gp))
+    assert_equal(1000, main.value(:pp))
+    main.change_value(:sp, 12)
+    assert_equal(12, main.value(:sp))
+  end
+
+  def test_format
+    main = CurrencySystem.new
+    assert_equal('5gp', main.stringify_values([0, 0, 5, 0]))
+    assert_equal('1cp 5sp 84gp', main.stringify_values([1, 5, 84, 0]))
+    assert_equal('93pp', main.stringify_values([0, 0, 0, 93]))
+    assert_equal('5cp 2pp', main.stringify_values([5, 0, 0, 2]))
+    assert_equal('3sp 56pp', main.stringify_values([0, 3, 0, 56]))
+  end
+
+  def test_parse
+    main = CurrencySystem.new
+    whitespace = lambda do
+      rand > 0.25 ? (' ' * rand(10)) : "\n"
+    end
+    10_000.times do
+      values = [rand(100), rand(100), rand(100), rand(100)]
+      labels = %i[cp sp gp pp]
+      to_parse = values.each_with_index.reduce('') do |string, (value, index)|
+        string + whitespace.call + value.to_s + whitespace.call + labels[index].to_s
+      end
+      assert_equal(values, main.parse_values(to_parse))
+      assert_equal(values[0] + (values[1] * 10) + (values[2] * 100) + (values[3] * 1000), main.parse_total_value(to_parse))
+    end
+  end
+
+  def test_split_total
+    main = CurrencySystem.new
+    total1 = 1215
+    assert_equal([5, 1, 2, 1], main.split_total(total1))
+    total2 = 7
+    assert_equal([7, 0, 0, 0], main.split_total(total2))
+    total3 = 142_809
+    assert_equal([9, 0, 8, 142], main.split_total(total3))
+  end
+
+  def test_custom_systems
+    odd_system_hash = {
+      penny: 1,
+      nickel: 5,
+      dime: 10,
+      quarter: 25,
+      shellfish: 33,
+      pencil: 73,
+      ruby: 750
+    }
+    odd_system = CurrencySystem.new(odd_system_hash)
+    odd_system.change_value(:ruby, 734)
+    whitespace = lambda do
+      rand > 0.25 ? (' ' * rand(10)) : "\n"
+    end
+    10_000.times do
+      values = [rand(100), rand(100), rand(100), rand(100), rand(100), rand(100), rand(100)]
+      labels = odd_system_hash.keys
+      to_parse = values.each_with_index.reduce('') do |string, (value, index)|
+        string + whitespace.call + value.to_s + whitespace.call + labels[index].to_s
+      end
+      assert_equal(values, odd_system.parse_values(to_parse))
+    end
+    total = 501_189
+    assert_equal([2, 1, 1, 0, 0, 8, 682], odd_system.split_total(total))
+  end
+end
--- world/economy/currency_system.rb
+++ world/economy/currency_system.rb
@@ -2,7 +2,7 @@
 
 # Handles numerical values of different parts of a currency system (default is the standard D&D system).
 class CurrencySystem
-  def init(currency_hash = {
+  def initialize(currency_hash = {
     cp: 1,
     sp: 10,
     gp: 100,
@@ -13,45 +13,45 @@ class CurrencySystem
   end
 
   def rebuild_data_from_hash
-    @currency_values_ordered = currency_hash.values.sort
-    @currency_labels_ordered = currency_hash.keys.sort do |key1, key2|
+    @currency_values_ordered = @currency_value.values.sort
+    @currency_labels_ordered = @currency_value.keys.sort do |key1, key2|
       @currency_value[key1] <=> @currency_value[key2]
     end
     # Sample produced regex for standard D&D system: /\A\s*(?:(\d+)\s*cp)?\s*(?:(\d+)\s*sp)?\s*(?:(\d+)\s*gp)?\s*(?:(\d+)\s*pp)?\s*\z/i
     regex_str = '\A\s*'
     @currency_labels_ordered.each do |label|
-      regex_str += '(?:(\d+)\s*' + Regexp.quote(label) + ')? \s*'
+      regex_str += '(?:(\d+)\s*' + Regexp.quote(label) + ')?\s*'
     end
     regex_str += '\z'
     @currency_regex = Regexp.new(regex_str, true)
   end
 
   def parse_total_value(string)
-    values = string.scan(currency_regex)
-    values.each_with_index.inject(0) do |total_value, (amount, index)|
-      total_value + amount * currency_values_ordered[index]
+    values = string.scan(@currency_regex)[0]
+    values.each_with_index.inject(0) do |totaled, (amount, index)|
+      totaled + amount.to_i * @currency_values_ordered[index]
     end
   end
 
   def parse_values(string)
-    string.scan(currency_regex)
+    string.scan(@currency_regex)[0].map(&:to_i)
   end
 
   def stringify_values(values)
     string = ''
     values.each_with_index do |value, index|
-      string += "#{value}#{@currency_labels_ordered[index]} "
+      string += "#{value}#{@currency_labels_ordered[index]} " unless value.zero?
     end
-    string
+    string.strip
   end
 
   def split_total(value)
     used_currency = []
-    @currency_value_ordered.length.times do
+    @currency_values_ordered.length.times do
       used_currency.push(0)
     end
-    currency_labels_ordered.reverse.each_with_index do |label, index|
-      index = currency_labels_ordered.length - index
+    @currency_labels_ordered.reverse.each_with_index do |label, index|
+      index = (@currency_labels_ordered.length - 1) - index
       used_currency[index] = value / @currency_value[label]
       value = value % @currency_value[label]
     end
