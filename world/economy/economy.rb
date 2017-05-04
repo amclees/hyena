b6d58a798a8b9e098d8a876598200d851053252f
amclees <andrewmclees5@gmail.com>
2017-05-03 19:06:13 -0700

    Add economy and city initial definitions

--- world/economy/economy.rb
+++ world/economy/economy.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: false
+
+# Economy is a wrapper for CurrencySystem that provides functions for varying prices and currency values according to the world state.
+class Economy
+  def initialize(currency_system = CurrencySystem.new)
+    @currency_system = currency_system
+  end
+end
--- world/map/city.rb
+++ world/map/city.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: false
 
-require_relative './'
+# A City is a single economic node in the map.
+class City
+  def initialize(currency_system = CurrencySystem.new, options = {})
+    default_options = {
+      population: 1000,
+      midpoint_population: 12_000,
+      economy_per_population_multiplier: 0.1
+    }
+    options.merge!(default_options)
+    @currency_system = currency_system
+    @population = default_options[:population]
+    @midpoint_population = options[:midpoint_population]
+    @economy_per_population_multiplier = options[:economy_per_population_multiplier]
+  end
+
+  def economic_impact
+    (@population / @midpoint_population) * @population * @economy_per_population_multiplier
+  end
+end
