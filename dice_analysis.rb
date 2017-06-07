58e62da77c7640d217e47d6848a2656cdca91b0d
amclees <andrewmclees5@gmail.com>
2017-06-06 17:18:45 -0700

    Add JSON output to dice analysis utility

--- dice_analysis.rb
+++ dice_analysis.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: false
 
+require 'json'
 require_relative './dice.rb'
+require_relative './json_manager.rb'
+
+JSONManager.init('data')
 
 puts 'Number of dice:'
 dice = gets.chomp.to_i
@@ -49,3 +53,5 @@ distribution.keys.sort.each do |key|
   distribution_table += "#{key} - #{distribution[key]} - #{(100.0 * (distribution[key].to_f / tests.to_f)).round(4)}\n"
 end
 puts distribution_table
+puts 'Enter a distribution name for the JSON to be stored under:'
+JSONManager.write_json('dice_distributions', "#{gets.chomp.strip}.json", distribution.to_json)
